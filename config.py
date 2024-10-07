36b4202d3fce2649da16a9a688aa048c14fe8a8c
rohan2608 <65375548+rohan2608@users.noreply.github.com>
2024-10-07 23:47:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env_vars = {
   #Get For @BotFather
   "BOT_TOKEN": "7421937596:AAGz19eTDaHYTsvxJUsi4JuADgZcVjLMGBI",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "psql 'postgresql://postgres:t7kDBeWbLKBiGKEt@healthfully-warm-possum.data-1.use1.tembo.io:5432/postgres'",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:t7kDBeWbLKBiGKEt@healthfully-warm-possum.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
   "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
